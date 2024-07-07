800677ab1e58b99b06e196df0900c366da5123b9
tyuukau <bachocnhi2002@gmail.com>
2024-07-07 15:18:07 +0700

    [src] ~ __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from ingestion import get_vector_db
-from model import get_huggingface_llm
+from .ingestion import get_vector_db
+from .model import get_huggingface_llm
 
 __all__ = [
     "get_vector_db",
